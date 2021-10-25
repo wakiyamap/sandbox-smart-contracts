354582d9fadb4742555dc14d2d56462410d4ac26
Andres Adjimann <andres@sandbox.game>
2021-10-25 14:52:38 -0300

    feat: add a setter for baseUrl, update doc

--- test/avatar/avatar.e2e.test.ts
+++ test/avatar/avatar.e2e.test.ts
@@ -2,33 +2,35 @@ import hre from 'hardhat';
 import {expect} from 'chai';
 import {getAvatarContracts} from '../common/fixtures/avatar';
 
-describe('@e2e @l1 roles', function () {
-  before(async function () {
-    const {l1, buyer} = await getAvatarContracts(
-      hre,
-      hre.companionNetworks['l2']
-    );
-    this.l1 = l1;
-    this.buyer = buyer;
-  });
-  it('admin', async function () {
-    const defaultAdminRole = await this.l1.avatar.DEFAULT_ADMIN_ROLE();
-    expect(await this.l1.avatar.hasRole(defaultAdminRole, this.l1.sandAdmin)).to
-      .be.true;
-  });
-  it('minter', async function () {
-    const minterRole = await this.l1.avatar.MINTER_ROLE();
-    expect(
-      await this.l1.avatar.hasRole(
-        minterRole,
-        this.l1.mintableERC721Predicate.address
-      )
-    ).to.be.true;
-  });
+describe('@e2e @l1 Avatar', function () {
+  describe('roles', function () {
+    before(async function () {
+      const {l1, buyer} = await getAvatarContracts(
+        hre,
+        hre.companionNetworks['l2']
+      );
+      this.l1 = l1;
+      this.buyer = buyer;
+    });
+    it('admin', async function () {
+      const defaultAdminRole = await this.l1.avatar.DEFAULT_ADMIN_ROLE();
+      expect(await this.l1.avatar.hasRole(defaultAdminRole, this.l1.sandAdmin))
+        .to.be.true;
+    });
+    it('minter', async function () {
+      const minterRole = await this.l1.avatar.MINTER_ROLE();
+      expect(
+        await this.l1.avatar.hasRole(
+          minterRole,
+          this.l1.mintableERC721Predicate.address
+        )
+      ).to.be.true;
+    });
 
-  it('trusted forwarder', async function () {
-    expect(await this.l1.avatar.getTrustedForwarder()).to.be.equal(
-      this.l1.trustedForwarder.address
-    );
+    it('trusted forwarder', async function () {
+      expect(await this.l1.avatar.getTrustedForwarder()).to.be.equal(
+        this.l1.trustedForwarder.address
+      );
+    });
   });
 });
