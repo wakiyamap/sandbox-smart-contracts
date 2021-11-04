ec66483ff510ad92bc3eb7afca1b2eb8279649d6
Andres Adjimann <andres@sandbox.game>
2021-11-04 14:01:44 -0300

    test: upgrade mumbai avatar implementation

---
+++ deploy_polygon/05_avatar/02_deploy_avatar_sale_v2.ts
@@ -0,0 +1,26 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {skipUnlessTestnet} from '../../utils/network';
+
+const func: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+): Promise<void> {
+  const {deployments, getNamedAccounts} = hre;
+  const {deployer, upgradeAdmin} = await getNamedAccounts();
+  await deployments.deploy('PolygonAvatarSale', {
+    from: deployer,
+    contract: 'AvatarSale',
+    log: true,
+    skipIfAlreadyDeployed: true,
+    proxy: {
+      owner: upgradeAdmin,
+      proxyContract: 'OptimizedTransparentProxy',
+      upgradeIndex: 1,
+    },
+  });
+};
+
+export default func;
+func.tags = ['PolygonAvatarSaleV2', 'PolygonAvatarSaleV2_deploy'];
+func.dependencies = ['PolygonAvatarSale_deploy'];
+func.skip = skipUnlessTestnet;
